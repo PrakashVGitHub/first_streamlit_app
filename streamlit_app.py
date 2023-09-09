b30ac4d20db4cb7fb3897628ce082cea7ecb19cf
PrakashVGitHub <144548456+PrakashVGitHub@users.noreply.github.com>
2023-09-09 14:23:46 -0400

    Update streamlit_app.py
    Added BF menu etc

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ import streamlit
 streamlit.title('My New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-
-streamlit.text('Omega 3 foods')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
